77d07c02bee9a41edfeb2ba236d41dd113c81b7c
marco decarlo <marco.decarlo@dechit.it>
2021-01-28 17:12:46 +0100

    footer & header

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    
+    <Header />
       <div className="container-fluid">
         Ciao
       </div>
+    <Footer />
     </>
   );
 }
