a576a5721869cbba56de1382511bec5b1747e9dd
marco decarlo <marco.decarlo@dechit.it>
2021-01-28 17:41:28 +0100

    install bootstrap-v5

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import configureStore from "./redux/configureStore";
 import { Provider as ReduxProvider } from "react-redux";
