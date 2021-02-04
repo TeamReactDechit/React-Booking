9add325828ef9243f209d663f6c9745bcbdbd090
matteoraccone <matteo.raccone@gmail.com>
2021-02-04 14:57:28 +0100

    home page layout fix

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,10 +5,10 @@ import SeatSearch from "../components/SeatSearch";
 const HomePage = () => {
   return (
     <div className="row">
-      <div className="container-fluid pt-4">
+      <div className="col-12 px-0 pt-4">
         <ConCarousel />
       </div>
-      <div className="container-fluid pt-2">
+      <div className="col-12 px-0 pt-2">
         <SeatSearch />
       </div>
     </div>
